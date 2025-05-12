33daeb3e568c7aac2167367d2fc3544d3c585fbf
MatviyEgorov23 <149804295+MatviyEgorov23@users.noreply.github.com>
2025-05-12 11:33:43 +0300

    Add files via upload

---
+++ Views/Orders/Orders.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_AuthenticationSystem_.Views.Orders
+{
+    public class ClientOrdersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
